43d9f33bc30dfa853e276924404cdb5f03db039f
ringself <ringself@163.com>
2015-08-27 11:44:37 +0800

    Update _webpack.config.js

--- app/templates/_webpack.config.js
+++ app/templates/_webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/,   loader: 'jsx-loader' },
+      { test: /\.js$/, exclude: /(node_modules|bower_components)/,  loader: 'babel-loader' },
       { test: /\.css$/,  loader: 'style!css' },
       { test: /\.scss$/, loader: 'style!css!sass?outputStyle=expanded' }
     ]
